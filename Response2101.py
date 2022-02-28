0458a2615d2229e9d41096c101eb1fd6456e47bf
shiri rave <shuroo1@hotmail.com>
2022-02-28 02:18:24 +0200

    Trying to apply a new reading data method +  fixing the size calculation

--- Response2101.py
+++ Response2101.py
@@ -1,34 +1,49 @@
 
 from Response import Response
 from Utils import Utils
+from Constants import Constants
 import struct
 # For the fetch clients list operation:
 class Response2101(Response):
 
+
+    def calcRespSize(self):
+        sze = len(self._clients_list) *(16+256);
+        return sze;
+
     def __init__(self, clients_list):
         # todo: add 2101 to constants
-        super().__init__(2101, clients_list)
-        self.client_list = clients_list ;
+        self._response_code, self._version, self._payload = '2101', Constants.version, clients_list;
+        self._clients_list = clients_list ;
         self._payload_size = self.calcRespSize();
 
-        def calcRespSize(self):
-            # or 255??
-            sze = len(self.clients_list) *(16+256);
-            return sze;
+    def pack_clients_list(self):
+        data = bytearray();
+        for row in self._clients_list:
+            clientId = row[0]
+            userName = Utils.strFiller(row[1],256)
+            print("uid:::",clientId , ",userName::" , userName )
+            data += struct.pack('<256s', Utils.strToBytes(str(userName)))
+            data += struct.pack('<16s', clientId)
 
-        def packResponse(self):
-            data = b''
-            if (self._payload_size == 0):
-                headerStruct = struct(f'< B H I')
-                data = headerStruct.pack(self._version, self._response_code, self._payload_size)
-                return data;
-            headerStruct = struct(f'< B H I  {self._payload_size}s')
-            data+= headerStruct.pack(self._version,self._response_code,self._payload_size,self._payload)
-            for client in clients_list:
-                clientId = client[0]
-                userName = bytes(client[1],'ascii')# todo: put in utils #Utils.strFiller(row[1], 256)
-                print("usename len:",len(userName))
-                print("uid:::", clientId, ",userName::", userName)
-                data += struct.pack('<256s', userName)
-                data += struct.pack('<16s', clientId)
+    # for 2101:
+    def pack_response(self):
+        print("Inside Child1")
+        data = b''
+        headerStruct = struct.Struct(f'< 4s 1s 4s')
+        #             headerStruct = struct.Struct(f'< H B I')
+        #             data = headerStruct.pack(self._response_code, self._version, self._payload_size)
+        #             return data;
+        data = headerStruct.pack(Utils.uncodeIntAsString(self._response_code), Utils.uncodeIntAsString(self._version),
+                                 Utils.uncodeIntAsString(self._payload_size))
+        if (self._payload_size == 0):
+            # data = headerStruct.pack(self._response_code, self._version, self._payload_size)
             return data;
+        for client in self._clients_list:
+            clientId =  client[0];
+            userName = bytes(Utils.strFiller(client[1],256),'ascii')# todo: put in utils #Utils.strFiller(row[1], 256)
+            print("usename len:",len(userName))
+            print("uid:::", clientId, ",userName::", userName)
+            data += struct.pack('<256s', userName)
+            data += struct.pack('<16s', clientId)
+        return data;
--- Utils.py
+++ Utils.py
@@ -12,5 +12,9 @@ class Utils:
     def strToBytes(content):
        return bytes(content, 'UTF-8');
 
+    @staticmethod
+    def uncodeIntAsString(integer):
+        return Utils.strToBytes(str(integer));
+
 # UTF-16LE: A character encoding that maps code points of Unicode character set to a sequence of 2 bytes (16 bits).
 # UTF-16LE stands for Unicode Transformation Format - 16-bit Little Endian.
